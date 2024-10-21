2657958c3594407f4edafbed2513ff08d88cca38
Angus Hollands <goosey15@gmail.com>
2024-10-21 14:50:14 +0100

    chore: add lint

--- src/roles.ts
+++ src/roles.ts
@@ -1,7 +1,7 @@
 import type { Parent, Literal, PhrasingContent } from 'mdast';
 
 // Custom in-line behavior
-export interface Role extends Literal, Parent  {
+export interface Role extends Literal, Parent {
   name: string;
   type: 'mystRole';
   value: string;
